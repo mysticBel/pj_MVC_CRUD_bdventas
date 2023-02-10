c1e6c9eb7658dd96aa02bb245fc0cfcd2362e45f
Maribel Maza <80657076+mysticBel@users.noreply.github.com>
2023-02-10 06:47:19 -0500

    crear clase ClienteModel dentro de carpeta Models

---
+++ sln_MVC_CRUD_bdventas/pj_MVC_CRUD_bdventas/Models/ClienteModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pj_MVC_CRUD_bdventas.Models
+{
+    public class ClienteModel
+    {
+        public int cod_cli { get; set; }
+        public string nom_cli { get; set; }
+        public string tel_cli { get; set; } // celular
+        public string cor_cli { get; set; }
+
+        public string dir_cli { get; set; }
+        public int cred_cli { get; set; }
+
+        public DateTime fec_nac { get; set; }
+
+        public int cod_dist { get; set; }
+        public string eli_cli { get; set; }
+    }
+}
---
+++ sln_MVC_CRUD_bdventas/pj_MVC_CRUD_bdventas/Models/DistritoModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pj_MVC_CRUD_bdventas.Models
+{
+    public class DistritoModel
+    {
+    }
+}
